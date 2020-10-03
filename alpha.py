c29003a6c5494f78d7b1436547f40978bf97a2d4
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-10-03 14:31:32 -0500

    Added ceaser shift

--- alpha.py
+++ alpha.py
@@ -1,22 +1,30 @@
+from nltk.corpus import words
 
-def decrypt(toDecode):
+def encrypt(toDecode):
     toDecode = toDecode.lower()
+    dictionary = {}
 
     for toShift in range(26):
         total = ""
-        # Iterate over toDecode
         for letter in toDecode:
-            # If the original input is a letter
             if 96 < ord(letter) < 123:
                 letter = ord(letter) + toShift
-                # Check for wrap around
                 if letter > 122:
                     letter = letter - 26
                 total += chr(letter)
-            # If the original input is not a letter, than assume whitespace
             else:
                 total += " "
-        print(toShift, total)
+
+        dictionary[toShift] = total
+
+    return dictionary
+
+def break_caesar(encryptions):
+    for shift, value in encryptions.items():
+        if (value in words.words()):
+            return {shift: value}
+
 
 def decrpyt_alpha(sent_word): # main function
-    return decrypt(sent_word)
+    encryptions = encrypt("ifmmp")
+    return break_caesar(encryptions)
