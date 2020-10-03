2f4bc6c0a95f9aeb8977d6d7fd2f501a293c24e8
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-10-03 12:02:05 -0500

    Added decript FIXME

--- alpha.py
+++ alpha.py
@@ -1,2 +1,17 @@
-def decrpyt_alpha(sent_word):
-    return "Output message"
+def decrypt(toDecode):
+    toDecode = toDecode.lower()
+
+    for toShift in range(26):
+        total = ""
+        for letter in toDecode:
+            if 96 < ord(letter) < 123:
+                letter = ord(letter) + toShift
+                if letter > 122:
+                    letter = letter - 26
+                total += chr(letter)
+            else:
+                total += " "
+        print(toShift, total)
+
+def decrpyt_alpha(sent_word): # main function
+    return decrypt(sent_word)
