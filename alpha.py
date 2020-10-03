2b96a634c3e744e569039fe376edc5ab9071dddb
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-10-03 12:16:11 -0500

    Added comments

--- alpha.py
+++ alpha.py
@@ -1,14 +1,19 @@
+
 def decrypt(toDecode):
     toDecode = toDecode.lower()
 
     for toShift in range(26):
         total = ""
+        # Iterate over toDecode
         for letter in toDecode:
+            # If the original input is a letter
             if 96 < ord(letter) < 123:
                 letter = ord(letter) + toShift
+                # Check for wrap around
                 if letter > 122:
                     letter = letter - 26
                 total += chr(letter)
+            # If the original input is not a letter, than assume whitespace
             else:
                 total += " "
         print(toShift, total)
