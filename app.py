4e169897fa797afea9da8a66e43d55966dacc9ee
alice-revelant <alice.byrnes@revelanttech.com>
2020-10-02 22:58:05 -0500

    Continued organization

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from werkzeug.utils import secure_filename
 import os
 import subprocess
 
+import alpha
+import golay
+import rm
+
 # UPLOAD_FOLDER = 'static'
 # ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -15,7 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route('/alphabetic', methods=['GET', 'POST'])
-def troubleshootFailed():
+def alphabetic():
     if request.method == 'GET':
         try:
             return render_template('alphabetic.html', message="Enter your code (with spaces)")
@@ -24,13 +28,33 @@ def troubleshootFailed():
     else:
         return render_template('alphabetic.html', message="Output here")
 
-@app.route('/binary')
-def troubleshootQuality():
+@app.route('/binary', methods=['GET', 'POST'])
+def binary():
     try:
-        return render_template('binary.html')
+        return render_template('binary.html', message="Enter your binary string here")
     except:
         return 'There was a problem redirecting'
 
+
+@app.route('/binary/golay', methods=['GET', 'POST'])
+def golay():
+    if request.method == 'GET':
+        try:
+            return render_template('golay.html', message="Enter your binary string here")
+        except:
+            return 'There was a problem redirecting'
+    else:
+        return render_template('golay.html', message="Output here")
+
+@app.route('/binary/rm', methods=['GET', 'POST'])
+def rm():
+    if request.method == 'GET':
+        try:
+            return render_template('rm.html', message="Enter your binary string here")
+        except:
+            return 'There was a problem redirecting'
+    else:
+        return render_template('rm.html', message="Output here")
 
 @app.route('/', methods=['GET'])
 def upload_file():
