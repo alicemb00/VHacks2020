5c01f564869e5a113f4e915548a45631081eb5bd
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-10-03 21:20:10 -0500

    Added binary search

--- alpha.py
+++ alpha.py
@@ -3,4 +3,5 @@ from break_caesar import encrypt, break_caesar
 
 def decrpyt_alpha(sent_word): # main function
     encryptions = encrypt(sent_word)
-    return break_caesar(encryptions)
+    decryptedText, shift = break_caesar(encryptions)
+    return decryptedText # Please include shift
